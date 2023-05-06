34b3886340c05137f6460ed6018d3d556ba1240e
Guilherme <guilhermemagno09@gmail.com>
2023-05-06 20:20:59 -0300

    adding mockdata in a single file

---
+++ src/components/helper/constants.ts
@@ -0,0 +1,143 @@
+import FilesIcon from "../icons/FilesIcon";
+import GithubIcon from "../icons/GithubIcon";
+import CodeIcon from "../icons/CodeIcon";
+import MailIcon from "../icons/MailIcon";
+import AccountIcon from "../icons/AccountIcon";
+import SettingsIcon from "../icons/SettingsIcon";
+
+export const sidebarTopItems = [
+  {
+    Icon: FilesIcon,
+    path: "/",
+  },
+  {
+    Icon: GithubIcon,
+    path: "/github",
+  },
+  {
+    Icon: CodeIcon,
+    path: "/projects",
+  },
+  {
+    Icon: MailIcon,
+    path: "/contact",
+  },
+];
+
+export const sidebarBottomItems = [
+  {
+    Icon: AccountIcon,
+    path: "/about",
+  },
+  {
+    Icon: SettingsIcon,
+    path: "/settings",
+  },
+];
+
+export const tabs = [
+    {
+      id: 1,
+      icon: "/images/react_icon.svg",
+      filename: "home.jsx",
+      path: "/",
+    },
+  
+    {
+      id: 2,
+      icon: "/images/react_icon.svg",
+  
+      // icon: "/images/html_icon.svg",
+      filename: "about.html",
+      path: "/about",
+    },
+    {
+      id: 3,
+      icon: "/images/react_icon.svg",
+  
+      // icon: "/images/yml_icon.svg",
+      filename: "contact.yml",
+      path: "/contact",
+    },
+    {
+      id: 4,
+      icon: "/images/react_icon.svg",
+  
+      // icon: "/images/py_icon.svg",
+      filename: "projects.py",
+      path: "/projects",
+    },
+    {
+      id: 5,
+      icon: "/images/react_icon.svg",
+      // icon: "/images/markdown_icon.svg",
+      filename: "github.md",
+      path: "/github",
+    },
+  ];
+
+
+
+export const explorerItems = [
+    {
+      id: 1,
+      icon: "/images/react_icon.svg",
+      filename: "home.jsx",
+      path: "/",
+    },
+    {
+      id: 2,
+      icon: "/images/react_icon.svg",
+  
+      // icon: "/images/html_icon.svg",
+      filename: "about.html",
+      path: "/about",
+    },
+    {
+      id: 3,
+      icon: "/images/react_icon.svg",
+      // icon: "/images/yml_icon.svg",
+      filename: "contact.yml",
+      path: "/contact",
+    },
+    {
+      id: 4,
+      icon: "/images/react_icon.svg",
+  
+      // icon: "/images/py_icon.svg",
+      filename: "projects.py",
+      path: "/projects",
+    },
+    {
+      id: 5,
+      icon: "/images/react_icon.svg",
+  
+      // icon: "/images/markdown_icon.svg",
+      filename: "github.md",
+      path: "/github",
+    },
+  ];
+  
+
+export const contactItems = [
+    {
+      social: "Email",
+      link: "guilhermemagno09@gmail.com",
+      href: "mailto:guilhermemagno09@gmail.com",
+    },
+    {
+      social: "LinkedIn",
+      link: "linkedin.com/in/guilherme-dso",
+      href: "https://www.linkedin.com/in/guilherme-dso/",
+    },
+    {
+      social: "GitHub",
+      link: "github.com/Guilherme-so",
+      href: "https://github.com/Guilherme-so",
+    },
+    {
+      social: "Website",
+      link: "gkos.tech",
+      href: "https://gkos.tech",
+    },
+  ];

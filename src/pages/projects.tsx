c4635e4159b61c4ec356650c6c29f36187722658
Guilherme <guilhermemagno09@gmail.com>
2023-05-07 10:22:34 -0300

    finishing projects page

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,23 +1,21 @@
 import React from "react";
 import { projects } from "@/components/helper/constants";
-import { Container,Heading } from "@/styles/projects";
+import { Container, Heading, Works } from "@/styles/projects";
 import ProjectCard from "@/components/projectCart";
 
 function Projects() {
   return (
-    <>
+    <Container>
       <Heading>Projects</Heading>
       <br />
       <hr />
-      <Container>
-        {projects.map((project,index) => {
-          return (
-            <ProjectCard key={index} {...project} />
-          );
+      <Works>
+        {projects.map((project, index) => {
+          return <ProjectCard key={index} {...project} />;
         })}
-      </Container>
+      </Works>
       <br />
-    </>
+    </Container>
   );
 }
 
--- src/styles/projects.ts
+++ src/styles/projects.ts
@@ -1,19 +1,9 @@
 import styled from "styled-components";
 
-export const Heading = styled.div`
-padding-top: 20px;
-display: block;
-color: #fff;
-`
-
 export const Container = styled.div`
-  padding: 1.5rem 20px 120px 20px;
-  display: grid;
+  padding: 1.5rem 20px 60px 20px;
   height: calc(100vh - 30px - 70px);
   overflow-x: auto;
-  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
-  gap: 1.5rem;
-
 
   ::-webkit-scrollbar {
     width: 8px;
@@ -33,10 +23,23 @@ export const Container = styled.div`
   ::-webkit-scrollbar-thumb:hover {
     background: rgb(30, 138, 189);
   }
+`;
 
+export const Heading = styled.div`
+  padding-top: 20px;
+  display: block;
+  font-size: x-large;
+  color: #fff;
+`;
 
-  @media screen and (max-width: 600px){
+export const Works = styled.div`
+  margin-top: 40px;
+  display: grid;
+  overflow-x: auto;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  gap: 1.5rem;
+
+  @media screen and (max-width: 600px) {
     grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
-
   }
-`
+`;

cb0f1f75f26f6ea2c816c8267a3a5f2f00c3f6a5
Guilherme <guilhermemagno09@gmail.com>
2023-05-08 17:46:58 -0300

    working on contactpage

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,25 +1,30 @@
 import React from "react";
 import { Container } from "../styles/contact";
 import { contactItems } from "@/components/helper/constants";
+import Head from "@/components/helper/head";
 
 function Contact() {
   return (
-    <Container>
-      <p>
-        <span>Guilherme</span>&#58;
-      </p>
-      <p>
-        &nbsp;&nbsp;&nbsp;&#8212; <span>socials</span>&#58;
-      </p>
-      {contactItems.map((item, index) => (
-        <p key={index}>
-          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{item.social}:{" "}
-          <a href={item.href} target="_blank" rel="noopener">
-            {item.link}
-          </a>
+    <>
+      <Head title="Contact" />
+
+      <Container>
+        <p>
+          <span>Guilherme</span>&#58;
         </p>
-      ))}
-    </Container>
+        <p>
+          &nbsp;&nbsp;&nbsp;&#8212; <span>socials</span>&#58;
+        </p>
+        {contactItems.map((item, index) => (
+          <p key={index}>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{item.social}:{" "}
+            <a href={item.href} target="_blank" rel="noopener">
+              {item.link}
+            </a>
+          </p>
+        ))}
+      </Container>
+    </>
   );
 }
 

a67eb13d59cb358837007e88c4a779b557003e5a
Guilherme <guilhermemagno09@gmail.com>
2023-05-08 17:47:40 -0300

    working on rojectspage

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,21 +1,45 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useRouter } from "next/router";
+import { useSelector } from "react-redux";
+import { RootState } from "@/redux/store";
 import { projects } from "@/components/helper/constants";
 import { Container, Heading, Works } from "@/styles/projects";
 import ProjectCard from "@/components/projectCart";
+import { enUS, ptBR } from "@/components/helper/internationalization";
+import Head from "@/components/helper/head";
 
 function Projects() {
+  const router = useRouter();
+  const { locale } = useSelector((state: RootState) => state.language.value);
+
+  useEffect(() => {
+    switch (locale) {
+      case "pt-BR":
+        router.push("/projects", "/projects", { locale: "pt-BR" });
+        break;
+      case "en-US":
+        router.push("/projects", "/projects", { locale: "en-US" });
+        break;
+    }
+  }, [locale]);
+
+  const translate = locale == "pt-BR" ? ptBR : enUS;
+
   return (
-    <Container>
-      <Heading>Projects</Heading>
-      <br />
-      <hr />
-      <Works>
-        {projects.map((project, index) => {
-          return <ProjectCard key={index} {...project} />;
-        })}
-      </Works>
-      <br />
-    </Container>
+    <>
+      <Head title="Projects" />
+      <Container>
+        <Heading>{translate.projectsPage.heading}</Heading>
+        <br />
+        <hr />
+        <Works>
+          {projects.map((project, index) => {
+            return <ProjectCard key={index} {...project} />;
+          })}
+        </Works>
+        <br />
+      </Container>
+    </>
   );
 }
 

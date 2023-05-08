61d56f5a2061baf7ccd76d4bcf599baa4f414b86
Guilherme <guilhermemagno09@gmail.com>
2023-05-08 17:46:37 -0300

    working on aboutpage

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,45 +1,67 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 import { useSelector } from "react-redux";
 import { RootState } from "@/redux/store";
 import SinglePage from "@/components/pdf/singlePagePDF";
 
 const Resume = "/images/resume.pdf";
 import { About } from "../styles/about";
+import { enUS, ptBR } from "@/components/helper/internationalization";
+import Head from "@/components/helper/head";
 
 const AboutPage = () => {
+  const router = useRouter();
   const sidebarWidth = useSelector((state: RootState) => state.files.size);
+  const { locale } = useSelector((state: RootState) => state.language.value);
+
+  useEffect(() => {
+    switch (locale) {
+      case "pt-BR":
+        router.push("/about", "/about", { locale: "pt-BR" });
+        break;
+      case "en-US":
+        router.push("/about", "/about", { locale: "en-US" });
+        break;
+    }
+  }, [locale]);
+
+  const translate = locale == "pt-BR" ? ptBR : enUS;
 
   return (
-    <About size={sidebarWidth}>
-      <h3>About Me</h3>
-      <br />
-      <ul>
-        <li>
-          <span role="img" aria-label="light-bulb">
-            💡
-          </span>{" "}
-          Open to collaborating on projects and innovative ideas.{" "}
-        </li>
-        <li>
-          <span role="img" aria-label="laptop">
-            💻
-          </span>{" "}
-          Currently studying fullstack development, Mobile and searching for a
-          full time job.{" "}
-        </li>
-      </ul>
-      <br />
-      <center>
-        <h3>
-          Resume (
-          <a href={Resume} download="resume_guilherme.pdf">
-            Download
-          </a>
-          )
-        </h3>
+    <>
+      <Head title="About" />
+
+      <About size={sidebarWidth}>
+        <h3>{translate.aboutPage.heading}</h3>
         <br />
-        <SinglePage pdf={Resume} />
-      </center>
-    </About>
+        <ul>
+          <li>
+            <span role="img" aria-label="laptop">
+              💻
+            </span>
+            {translate.aboutPage.first}
+          </li>
+          <li>
+            <span role="img" aria-label="light-bulb">
+              💡
+            </span>
+            {translate.aboutPage.second}
+          </li>
+        </ul>
+        <br />
+        <center>
+          <h3>
+            {translate.aboutPage.resume} (
+            <a href={Resume} download="resume_guilherme.pdf">
+              Download
+            </a>
+            )
+          </h3>
+          <br />
+          <SinglePage pdf={Resume} />
+        </center>
+      </About>
+    </>
   );
 };
 

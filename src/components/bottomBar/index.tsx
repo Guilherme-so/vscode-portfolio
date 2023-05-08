ab1479e325365e7ecd26717692bd23aeb44e9682
Guilherme <guilhermemagno09@gmail.com>
2023-05-07 23:33:06 -0300

    finishing footer

--- src/components/bottomBar/index.tsx
+++ src/components/bottomBar/index.tsx
@@ -1,13 +1,42 @@
 import React from "react";
+import { Footer, ListOne, ListTwo } from "./styled";
+import { ArrowsClockwise, Bell, Checks, GitBranch, User } from "phosphor-react";
+import ChevronRight from "../icons/ChevronRight";
 
 function BottomBar() {
   return (
-    <div
-      style={{
-        backgroundColor: "black",
-        height: "33px",
-      }}
-    ></div>
+    <Footer>
+      <ListOne>
+        <li>
+          <span>
+            <ChevronRight />
+          </span>
+          <span>
+            <ChevronRight />
+          </span>
+        </li>
+        <li>
+          <GitBranch size={18} />
+          <span>main*</span>
+        </li>
+        <li>
+          <ArrowsClockwise size={16} />
+        </li>
+      </ListOne>
+
+      <ListTwo>
+        <li>
+        <Checks size={18} color="white" />
+        <span>Prettier</span>
+        </li>
+        <li>
+          <User size={16} color="white" />
+        </li>
+        <li>
+          <Bell size={16} color="white" />
+        </li>
+      </ListTwo>
+    </Footer>
   );
 }
 
---
+++ src/components/bottomBar/styled.ts
@@ -0,0 +1,73 @@
+import styled from "styled-components";
+
+export const Footer = styled.footer`
+  border-top: 1px solid ${(props) => props.theme.color.bottombarborder};
+  background-color: ${(props) => props.theme.color.bottombarbg};
+  height: 26px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const ListOne = styled.ul`
+  display: flex;
+  color: #fff;
+  align-items: center;
+  font-size: 14px;
+
+  li:nth-child(1) {
+    background-color: rgb(189, 147, 249);
+    position: relative;
+    color: #000;
+    width: 37px;
+    height: 25px;
+    margin-right: 10px;
+
+    span:nth-child(1) {
+      font-size: 12px;
+      display: flex;
+      position: absolute;
+      top: 6px;
+      left: 7px;
+    }
+    span:nth-child(2) {
+      font-size: 12px;
+      display: flex;
+      position: absolute;
+      transform: rotate(185deg);
+      bottom: 6px;
+      right: 7px;
+    }
+  }
+
+  li:nth-child(2) {
+    display: flex;
+    align-items: center;
+    gap: 1px;
+    margin-right: 20px;
+  }
+
+  li:nth-child(3) {
+    display: flex;
+    align-items: center;
+    transform: rotate(280deg);
+  }
+`;
+
+export const ListTwo = styled.ul`
+  display: flex;
+  align-items: center;
+  gap: 20px;
+  margin-right: 15px;
+
+  li:nth-child(1) {
+    display: flex;
+    align-items: center;
+    gap: 4px;
+    
+    span {
+      color: #fff;
+      font-size: small;
+      letter-spacing: 0px;
+    }
+  }
+`;

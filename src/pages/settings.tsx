daf7a07a4d9fd859cb228c82c658e66b35d7f387
Guilherme <guilhermemagno09@gmail.com>
2023-05-07 23:40:56 -0300

    working on settings

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import {
+  Card,
+  Container,
+  ImageContainer,
+  ImageWrapper,
+} from "@/styles/settings";
+import Image from "next/image";
+import React from "react";
 
 function Settings() {
+  function setBeardedTheme() {
+    window.localStorage.setItem("theme", "bearded");
+    window.dispatchEvent(new Event("storage"));
+  }
+  function setDraculaTheme() {
+    window.localStorage.setItem("theme", "dracula");
+    window.dispatchEvent(new Event("storage"));
+  }
+
   return (
-    <div>Settings</div>
-  )
+    <Container>
+      <h2>Themes</h2>
+
+      <ImageContainer>
+        <Card>
+          <ImageWrapper>
+            <Image src="/images/bearded.png" alt="dracula theme" fill={true} />
+          </ImageWrapper>
+          <h3>Bearded</h3>
+          <h4>Bearded theme</h4>
+          <button onClick={() => setBeardedTheme()}>Set theme</button>
+        </Card>
+
+        <Card>
+          <ImageWrapper>
+            <Image src="/images/dracula.png" alt="dracula theme" fill={true} />
+          </ImageWrapper>
+          <h3>Dracula</h3>
+          <h4>Dracula theme</h4>
+          <button onClick={() => setDraculaTheme()}>Set theme</button>
+        </Card>
+      </ImageContainer>
+    </Container>
+  );
 }
 
-export default Settings
+export default Settings;
